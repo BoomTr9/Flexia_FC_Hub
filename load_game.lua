91aa76cbfc61b4a5109c1adba6516e90af18d632
BoomTr9 <140717689+BoomTr9@users.noreply.github.com>
2025-02-09 21:06:02 +0700

    Update load_game.lua

--- load_game.lua
+++ load_game.lua
@@ -1,5 +1,5 @@
 local pid = game.PlaceId
-local notilib = loadstring(game:HttpGet('https://pastebin.com/raw/SQzJ2zRP'))
+local notilib = loadstring(game:HttpGet('https://pastebin.com/raw/SQzJ2zRP'))()
 
 if pid == 125503319883299 or pid == 116597124551478 then
   notilib.Notify("Notification", "Game :Arcane-Conquest")
